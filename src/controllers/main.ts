8aa9daf51257be8b6e8241fa06db4bd92a97296e
Tinsae20 <tinsaeyirgu88@gmail.com>
2025-01-26 23:23:49 +0300

    setting up valdation decorator

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -2,6 +2,13 @@
 import { Request, Response, NextFunction } from "express";
 import { Controller } from "../decorators/controller";
 import { Route } from "../decorators/route";
+import Joi from "joi";
+import { Validate } from "../decorators/validate";
+
+const postCheckValidation = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email()
+})
 
 @Controller('')
 class MainController {
@@ -13,6 +20,7 @@ class MainController {
     }
 
     @Route('post', '/check')
+    @Validate(postCheckValidation)
     postCheck(req:Request, res:Response, next:NextFunction):any {
         logging.info('Check called successully')
         return res.status(200).json({...req.body})
